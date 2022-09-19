9a13d00f0d63ced77ca70d7230609ff1b9b7508f
Daniel Toth <78477459+danieltoth-sys@users.noreply.github.com>
2022-09-19 18:21:19 +0200

    Add project files.

---
+++ PythonTest/PythonTest.py
@@ -0,0 +1,35 @@
+#So in basic Python you have: Methods and variables. In the advanced version, you have so much more, but let's stick to the basics.
+#Methods are basically the main functions of the Python language. You can do awesome stuff with those.
+#Basic variables can be numbers and texts. Advanced variables can be objects and even methods. But that is really advanced stuff.
+
+#You write your comments with '#'. Comments are those texts that won't going to be ran by the compiler. Basically if want to write some stuff for your code for better understanding.
+#There is a difference between 'PRINT' and 'print'. First one is an error, the second one is a method. Be careful to write your code properly. Most of the code is lower-cased.
+#So every single line of code is unique and you cannot use 1 line for more stuff. For example you cannot write 2 methods in one line.
+
+#Write 'Hello world' to the screen:
+
+print("Hello world")
+
+#print is the method that we call for doing the writing. Every method looks like this: print()
+#                                                                                      command(parameters)
+#Parameters are mostly variables that the method wants to use. Here I used the "Hello world" text. Remember! If you want to write text, always use "" or ''.
+#You can of course use a variable.
+
+text = "Hello world" #Here I created (declared) a variable, which is a text type and it's value is "Hello world".
+
+print(text) #And here I use the print() method with the "text" variable.
+
+#Of course you can change the values for different results.
+
+#Add 2 numbers
+
+#Every variable is unique. You don't have to set the type of the variable (for example you don't have to set the variable to be number type), the compiler figures out the type of the variable.
+#And also. You cannot change the type of the variable that easily. If the variable is a number type, you have to use a method to change the type.
+a = 5 #Here the "a" variable's value is 5.
+b = 2 #Here the "b" variable's value is 2.
+
+#Here I use another variable to add those 2 numbers called "sum"
+
+sum = a + b #Here sum's value is a+b, so it should be 7 by default. You can change the values in the code for different results.
+
+print(sum) #Let's write the result to the screen.
